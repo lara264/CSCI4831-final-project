b496d767b0ef8710eba50174595d9b2dbe7016a4
Ryan Drew <learnitall0@gmail.com>
2021-04-15 19:39:39 -0600

    Create main module in csci4831final

---
+++ csci4831final/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Main module of csci4831final."""
+
+
+def main():
+    """Main entrypoint function."""
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
